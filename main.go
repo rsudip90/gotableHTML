df801d8cf3a5a996272ccc62b1b4dc1615260e01
rsudip90 <rsudip90.dev@gmail.com>
2017-03-04 19:22:56 +0530

    FIX: html5 string removed, as per table html return whole html document

--- main.go
+++ main.go
@@ -10,39 +10,9 @@ import (
 )
 
 func main() {
-	var html5 = `
-		<!DOCTYPE html>
-		<html lang="en">
-		<head>
-		<title>RentRoll Sample Report</title>
-		<style>
-			html, body{
-				margin: 0;
-				padding: 0;
-				font-family: Helvetica, sans-serif;
-				font-size: 14px;
-				background: #FAFAFA;
-			}
-			div{
-				display: block;
-			}
-			.container{
-				background-color: rgba(0,0,0,0);
-				padding: 10px 25px;
-			}
-		</style>
-		<link rel="stylesheet" type="text/css" href="report.css" />
-		</head>
-		<body>
-		<div class="container">
-			%s
-		</div>
-		</body>
-		</html>`
 
 	const (
-		lorem = `Lorem ipsum dolor sit amet, elementum fermentum suspendisse,
-		illum est curabitur a sem justo rhoncus. Ac iaculis nec amet nisl,
+		lorem = `Lorem ipsum dolor sit amet, elementum fermentum suspendisse,illum est curabitur a sem justo rhoncus. Ac iaculis nec amet nisl,
 		scelerisque sed quis nec dignissim dolor tempor, pellentesque tortor phasellus ut,
 		risus eros sed primis vestibulum, vestibulum viverra. Maecenas orci scelerisque.
 		Lectus cursus lorem etiam adipisicing, enim per tellus,
@@ -55,14 +25,14 @@ func main() {
 	t.SetTitle("Go Table")
 	t.AddColumn("Line No", 7, gt.CELLSTRING, gt.COLJUSTIFYLEFT)
 	t.AddColumn("Unit", 14, gt.CELLSTRING, gt.COLJUSTIFYLEFT)
-	t.AddColumn("Amount", 10, gt.CELLINT, gt.COLJUSTIFYRIGHT)
-	t.AddColumn("Description", 150, gt.CELLSTRING, gt.COLJUSTIFYLEFT)
+	t.AddColumn("Amount", 10, gt.CELLINT, gt.COLJUSTIFYLEFT)
+	t.AddColumn("Description", 80, gt.CELLSTRING, gt.COLJUSTIFYLEFT)
 	rs := t.CreateRowset()
 	for i := 0; i < 5; i++ {
 		t.AddRow()
 		t.Puts(-1, 0, strconv.Itoa(i+1))
 		t.Puts(-1, 1, "Unit - "+strconv.Itoa(i+1))
-		t.Puti(-1, 2, int64(i*10))
+		t.Puti(-1, 2, int64(i*1000))
 		t.Puts(-1, 3, lorem)
 		t.AppendToRowset(rs, i)
 	}
@@ -101,8 +71,11 @@ func main() {
 	t.SetHeaderCSS(c)
 
 	tableHTML, err := t.SprintTable(gt.TABLEOUTHTML)
-	// fit content in html5
-	tableHTML = fmt.Sprintf(html5, tableHTML)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
 	f, err := os.Create("table.html")
 	if err != nil {
 		fmt.Println(err.Error())
